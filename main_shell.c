1cd9b4009292e9df6e71b4d36d53604eeaeebdb1
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2022-08-30 09:06:40 -0700

    push

--- main_shell.c
+++ main_shell.c
@@ -1,1 +1,18 @@
-#include "main.h"
+#include "shell.h"
+
+/**
+ * main - main function that accepts input 
+ * @argc: command line argument count
+ * @agrv: command line vector count
+ * Return:
+ */
+
+int main (int argc __attribut__((unused)), char **agrv)
+{
+	char *line;
+	(void)argv; signal(SIGINT, ctrl_C);
+
+	while (1)
+	{
+		write("#shell$ ", STDOUT_FILENO);
+		line = _getline();
