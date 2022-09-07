3abd753b8c71ba6467379ed2fb2eb4b8b220f2ab
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-09-07 12:26:47 +0100

    Prints char and str

---
+++ print_string.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - function that prints stings.
+ * @s: pointer to string.
+ * Return: length of string.
+ */
+int print_string(char *str)
+{
+	char empty[] = "(null)";
+	int i, length;
+
+	if (str == NULL)
+	{
+		for (i = 0; empty[i]; i++)
+			_putchar(empty[i]);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	length = _strlen(str);
+	return (length);
+}
