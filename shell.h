b9446de2d752dc5749a71f3ca4b1e5fa97d92fea
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-09-07 14:00:07 +0100

    fix  prototypes

--- shell.h
+++ shell.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -38,19 +39,19 @@ typedef struct built
 extern char **environ;
 
 /*generic functions*/
-int _strlen(char *s);
+int _strlen(char *str);
 int print_string(char *str);
 char *_strdup(char *str);
 void _freearrp(char **av);
 void *_calloc(unsigned int nmemb, unsigned int size);
-char *str_concat(char *s1, char *s2);
-char *_strtok(char *s, char *delim);
+char *str_concat(char *str1, char *str2);
+char *_strtok(char *str, char *delim);
 char **_cpyarrp(char **arrp);
 int findenv(char **env, char *cmp_val);
 void print_err_numb(int n);
-int _strcmp(char *s1, char *s2);
-char *_strchr(char *s, char c);
-void rev_string(char *s);
+int _strcmp(char *str1, char *str2);
+char *_strchr(char *str, char c);
+void rev_string(char *str);
 
 /*shell functions*/
 char *read_line(char **env, err_t *errval);
